05e082c5190ccf693fa2c713caf537ac5aeef2f9
Stetson Harding <stetson2001@msn.com>
2021-09-08 20:48:00 -0700

    Snapshot Testing Fundementals

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,6 +1,8 @@
-import React from 'react';
-export default ({tags})=>(
-    <div>
-        {tags.map(tag=><code key={tag}>{tag}</code>)}
-    </div>
-)
+import React from "react";
+export default ({ tags }) => (
+  <div>
+    {tags.map((tag) => (
+      <code key={tag}>{tag}!</code>
+    ))}
+  </div>
+);
--- src/components/TagsList.spec.js
+++ src/components/TagsList.spec.js
@@ -1,16 +1,13 @@
-// import React from 'react';
-// import TagsList from './TagsList'
-// import renderer from 'react-test-renderer';
+import React from "react";
+import TagsList from "./TagsList";
+import renderer from "react-test-renderer";
 
-// describe("The tags list",()=>{
-/**
- * The tagsList can be tested against an expected snapshot value, as in below.
- */
-//    it ("renders as expected",()=>{
-//        const tree = renderer
-//            .create(<TagsList tags={[`css`,`html`,`typescript`,`coffeescript`]}/>)
-//            .toJSON();
+describe("The tags list", () => {
+  it("renders as expected", () => {
+    const tree = renderer
+      .create(<TagsList tags={["css", "html", "go"]} />)
+      .toJSON();
 
-//        expect(tree).toMatchSnapshot();
-//    });
-// });
+    expect(tree).toMatchSnapshot();
+  });
+});
