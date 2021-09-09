20014d6f6327b73a4342baefc9ae5104cc8a973a
Stetson Harding <stetson2001@msn.com>
2021-09-09 08:19:13 -0700

    Testing question detail component

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,55 +1,26 @@
-import { iteratee } from "lodash";
-import { describe } from "optimist";
+import { mapStateToProps } from "../QuestionDetail";
+// import { describe } from "optimist";
+// import { map } from "lodash";
 
 describe("The Question Detail Component", () => {
+  describe("The Container Element", () => {
+    describe("mapsStateToProps", () => {
+      it("should map the state to props correctly", () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: "Space is big",
+        };
+        const appState = {
+          questions: [sampleQuestion],
+        };
+        const ownProps = {
+          question_id: 42,
+        };
+        const componentsState = mapStateToProps(appState, ownProps);
+        console.log(componentsState);
+        expect(componentsState).toEqual(sampleQuestion);
+      });
+    });
+  });
   it("should not regress", () => {});
 });
-
-/**
- * In this file we explore testing a React Redux component by writing seperate tests for the display and the container
- */
-// import { QuestionDetailDisplay , mapStateToProps } from '../QuestionDetail'
-// import renderer from 'react-test-renderer';
-// import React from 'react';
-
-// describe("The question detail view",()=>{
-//     describe("The display component",()=>{
-//        it("Should not regress",()=>{
-//            const tree = renderer
-//                .create(
-//                    <QuestionDetailDisplay
-//                        title="The meaning of life"
-//                        body="42"
-//                        answer_count={0}
-//                        tags={["hitchhiking"]}
-//                    />
-//                );
-//            expect(tree.toJSON()).toMatchSnapshot();
-
-//        }) ;
-//     });
-
-/**
-     * Note that since Map State to Props is a pure function, we can easily test it here
-     * without needing to scaffold the entire application around it.
-//      */
-//     describe("Map state to props",()=>{
-//         const sampleQuestion = {
-//             question_id:42,
-//             body:"Space is big"
-//         };
-
-//         const appState = {
-//             questions:[sampleQuestion]
-//         };
-
-//         const ownProps = {
-//             question_id: 42
-//         };
-
-//         const componentState = mapStateToProps(appState,ownProps);
-
-//         expect(componentState).toEqual(sampleQuestion);
-
-//     });
-// });
