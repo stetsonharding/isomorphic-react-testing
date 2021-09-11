0e0890abaf1428f86e5610cc989494d11bf55f1c
Stetson Harding <stetson2001@msn.com>
2021-09-11 07:45:26 -0700

    created mock function for stateful component.

--- src/components/__tests__/NotificationsViewer.js
+++ src/components/__tests__/NotificationsViewer.js
@@ -3,13 +3,22 @@ import renderer from "react-test-renderer";
 import React from "react";
 import delay from "redux-saga";
 
-describe(
-  ("The notification viewer",
-  () => {
-    it("should display the correct number of notification", async () => {
-      const tree = renderer.create(<NotificationsViewer />);
+jest.mock("../../services/NotificationsService");
 
-      await delay();
-    });
-  })
-);
+const NotificationsService =
+  require("../../services/NotificationsService").default;
+describe("The notification viewer", () => {
+  beforeAll(() => {
+    NotificationsService.__setCount(42);
+  });
+
+  it("should display the correct number of notification", async () => {
+    const tree = renderer.create(<NotificationsViewer />);
+
+    await delay();
+    const instance = tree.root;
+    const comp = instance.findByProps({ className: "notifications" });
+    const text = comp.children[0];
+    expect(text).toEqual("5 Notifications Awaiting");
+  });
+});
